6b21e9348f33e56eae148c4fd758d27f6d3ff41c
Markov Alexander <markovalex2004@gmail.com>
2024-11-14 14:28:12 +0300

    config fixed at callbackservice

--- callback-service/config/config.py
+++ callback-service/config/config.py
@@ -3,7 +3,7 @@ from email.policy import default
 
 
 class Config:
-    SERVER_PORT: int = int(os.getenv("CALLBACK_PORT", default=8082))
+    SERVER_PORT: int = int(os.getenv("CALLBACK_PORT", default=8080))
 
     RABBIT_HOST: str = os.getenv("RABBIT_HOST", default="localhost")
     RABBIT_USER: str = os.getenv("RABBIT_USER", default="rmuser")
@@ -12,10 +12,10 @@ class Config:
     REDIS_HOST: str = os.getenv("REDIS_HOST", default="localhost")
     REDIS_PORT: str = os.getenv("REDIS_PORT", default="6379")
 
-    INPUT_QUEUE: str = os.getenv("CALLBACK_SERV_QUEUE", default="callback_input")
+    INPUT_QUEUE: str = os.getenv("CALLBACK_SERV_QUEUE", default="cb_queue")
 
     MAIL_SENDER_URL: str = os.getenv("MAIL_SENDER_URL", default="http://localhost:8081/default")
-    SMS_SENDER_URL: str = os.getenv("MAIL_SENDER_URL", default="http://localhost:8081/default")
+    SMS_SENDER_URL: str = os.getenv("SMS_SENDER_URL", default="http://localhost:8081/default")
 
     CALLBACK_SERVICE_TIMEOUT: int = int(os.getenv("CALLBACK_SERVICE_TIMEOUT", default=10))
     CALLBACK_SERVICE_RABBIT_TTL: int = int(os.getenv("CALLBACK_SERVICE_RABBIT_TTL", default=15))
