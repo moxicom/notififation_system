303bb94a1715c45c4d2fae60906ba11b9b67c85c
Markov Alexander <markovalex2004@gmail.com>
2024-11-14 16:14:33 +0300

    added token verification

--- gateway-service/adapters/httpserver/router.py
+++ gateway-service/adapters/httpserver/router.py
@@ -1,5 +1,8 @@
+import httpx
+
+from core.config import config
 from models import NotificationInfo
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Request, status, Depends
 from pydantic import BaseModel
 from adapters.rabbithandler import rabbit_producer
 
@@ -14,9 +17,27 @@ class SuccessResponse(BaseModel):
     message: str
 
 
-@router.post("/notify",
-             response_model=SuccessResponse)
-async def notify_handler(notification: NotificationInfo):
+async def verify_token(request: Request):
+    token = request.headers.get("Authorization")
+    if not token or not token.startswith("Bearer "):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Missing or invalid token")
+
+    token_value = token.split(" ")[1]
+
+    async with httpx.AsyncClient() as client:
+        response = await client.get(
+            config.AUTH_SERVICE_URL+"/api/v1/login",
+            headers={"Authorization": f"Bearer {token_value}"}
+        )
+
+    if response.status_code != 200 and response.status_code != 500:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Invalid token")
+    if response.status_code == 500:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="internal error")
+
+
+@router.post("/notify", response_model = SuccessResponse)
+async def notify_handler(notification: NotificationInfo, token_verified: bool = Depends(verify_token)):
     try:
         message = notification.model_dump(by_alias=True)
         rabbit_producer.send_message(message)
--- gateway-service/core/config/config.py
+++ gateway-service/core/config/config.py
@@ -7,6 +7,7 @@ class Config:
     RABBIT_PASSWORD: str = os.getenv("RABBIT_PASSWORD")
     CALLBACK_SERV_QUEUE: str = os.getenv("CALLBACK_SERV_QUEUE", default="callback_input")
     GATEWAY_INPUT_QUEUE: str = os.getenv("GATEWAY_INPUT_QUEUE", default="gateway_input")
+    AUTH_SERVICE_URL: str = os.getenv("AUTH_SERVICE_URL", default="http://auth_service:8000")
 
 
 config = Config()
